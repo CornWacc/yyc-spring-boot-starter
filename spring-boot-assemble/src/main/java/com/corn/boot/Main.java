bac5bcecfbc800b16c275af34ebd588e400b5df7
喻雨辰 <791621554@qq.com>
2019-01-25 15:56:39 +0800

    需改pom打包文件

--- spring-boot-assemble/src/main/java/com/corn/boot/Main.java
+++ spring-boot-assemble/src/main/java/com/corn/boot/Main.java
@@ -3,11 +3,17 @@ package com.corn.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
