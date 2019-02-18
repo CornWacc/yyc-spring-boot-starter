2cd6ed904716e36e3b3716f4b7cd945d30624e86
喻雨辰 <791621554@qq.com>
2019-02-18 17:06:11 +0800

    小修改

--- spring-boot-pojo/src/main/java/com/corn/boot/annotations/Runner.java
+++ spring-boot-pojo/src/main/java/com/corn/boot/annotations/Runner.java
@@ -2,6 +2,7 @@ package com.corn.boot.annotations;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 import java.lang.annotation.*;
@@ -10,7 +11,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public @interface Runner {
 
     String port() default "8080";
