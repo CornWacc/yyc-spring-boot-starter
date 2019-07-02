f4d56d8f1dac70aef248115a2e3d668f50592309
Yyc <791621554@qq.com>
2019-07-02 15:04:47 +0800

    修改JsonResult的构造方法

--- spring-boot-pojo/src/main/java/com/corn/boot/base/JsonResult.java
+++ spring-boot-pojo/src/main/java/com/corn/boot/base/JsonResult.java
@@ -1,10 +1,15 @@
 package com.corn.boot.base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author yyc
  * @apiNote 通用返回json类
  */
 public class JsonResult extends Base {
+
+    private static final Logger log = LoggerFactory.getLogger(JsonResult.class);
 
     public static final String SUCCESS_MSG = "SUCCESS";
 
@@ -27,7 +32,17 @@ public class JsonResult extends Base {
     }
 
     public JsonResult(Object object) {
-        this.object = object;
+
+        if(BaseRes.class.isAssignableFrom(object.getClass())){
+            this.object = object;
+            BaseRes res = (BaseRes) object;
+            this.status = res.getStatus().code();
+            this.msg = res.getMessage();
+        }else{
+            this.status = FIAL_MSG;
+            this.msg = "系统出错啦！！";
+            log.error("回调参数返回错误!");
+        }
     }
 
     public JsonResult(Object object, String msg) {
